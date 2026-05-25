d4e93cafaef3a47b5de258d9aaeeca3406538b4b
Your Name <you@example.com>
2026-05-25 12:12:41 -0500

    Add authentication DTOs and Usuario repository

---
+++ src/main/java/com/goodvision/dto/RegisterRequest.java
@@ -0,0 +1,73 @@
+package com.goodvision.dto;
+
+import com.goodvision.entity.Role;
+
+public class RegisterRequest {
+
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String telefono;
+    private String direccion;
+    private String password;
+    private Role role;
+
+    public RegisterRequest() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
